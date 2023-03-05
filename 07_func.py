ba63dabd47a903e52709c4950fbb9bbdcad9de4c
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-04 20:59:39 -0500

    07 funciones

---
+++ 07_func.py
@@ -0,0 +1,20 @@
+
+print('Hola')
+
+def my_print(text):
+    print(text*2)
+
+my_print('Este es mi texto')
+
+
+
+def my_print(text):
+    print(text*2)
+
+def suma(a,b):
+   my_print(a + b) 
+
+suma(1,2) # 6
+suma(33,33) # 132
+
+
