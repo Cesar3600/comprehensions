06450882cd76a8f65f0c054a45f7d29c2a67d80a
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-04 20:03:08 -0500

    06 dictionary comprehesion condition

---
+++ 06_dict.py
@@ -0,0 +1,23 @@
+
+import random
+
+countries = ['col','mex','bol','pe']
+
+population_v2 = {country:random.randint(1,100) for country in countries}
+print(population_v2)
+
+result = {country:population for (country,population) in population_v2.items() if population > 50}
+print(result)
+
+
+
+text="Hola soy cesar"
+
+unique = {c:c.upper() for c in text if c in 'aeiou'}
+print(unique)
+
+
+
+
+
+
