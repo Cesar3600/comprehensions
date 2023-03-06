87a66c2fb48b7f5ddbc34c71175e2499fa20dba9
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-05 19:19:55 -0500

    map

--- 13_hof.py
+++ 13_hof.py
@@ -18,8 +18,12 @@ increment_v2 = lambda x : x + 1
 hight_ord_func_v2 = lambda x,func : x + func(x) 
 
 result_v2 = hight_ord_func_v2(33, increment_v2)
+print(result_v2)
 
+result_v2 = hight_ord_func_v2(33, lambda x : x + 2)
+print(result_v2)
 
+result_v2 = hight_ord_func_v2(33, lambda x : x * 5)
 print(result_v2)
 
 
---
+++ 14_map.py
@@ -0,0 +1,25 @@
+########################
+numbers = [1,2,3,4]
+
+numbers_v2 =[]
+
+for num in numbers:
+    numbers_v2.append(num * 2)
+
+print(numbers)
+print(numbers_v2)
+
+#########################
+
+numbers_v3 = map(lambda num : num * 2, numbers)
+print(list(numbers_v3))
+
+
+##########################
+
+numbers_1 = [1,2,3,4]
+numbers_2 = [5,6,7]
+
+
+results = map(lambda x,y: x+y,numbers_1,numbers_2)
+print(list(results))
