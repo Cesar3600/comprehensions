4912450d9bcee558a1a897e33e65c7b8dba04e0c
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-03-09 14:28:51 -0500

    escribir en un archivo

---
+++ 24_write.py
@@ -0,0 +1,9 @@
+
+with open('./text.txt','w+') as file:
+    for line in file:
+        print(line)
+    file.write('Karla kush\n')
+    file.write('Scarlet Red\n')
+    file.write('Whitney WEstgate\n')
+
+
